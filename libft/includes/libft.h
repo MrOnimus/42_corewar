465d26dc0eb76a140ee507216dbff6c241d4fccf
immn <immnsn@hotmail.com>
2020-02-08 20:26:05 +0300

    tasks for MRONONIMUS

--- libft/includes/libft.h
+++ libft/includes/libft.h
@@ -178,6 +178,7 @@ unsigned int		ft_reverse_bits(unsigned int octet);
 ** STRING
 */
 
+char				del_str(char **line);
 int					ft_strncmp(const char *s1, const char *s2, size_t n);
 int					ft_strcmp(const char *s1, const char *s2);
 char				*ft_strnstr(const char *haystack, const char *needle,
